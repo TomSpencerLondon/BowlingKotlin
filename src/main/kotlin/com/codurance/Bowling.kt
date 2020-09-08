fd0af24a097a5e263da550b5bbfbfed6626d7550
Tom <thomas.spencer@codurance.com>
2020-09-08 18:05:00 +0100

    Initial commit

---
+++ src/main/kotlin/com/codurance/Bowling.kt
@@ -0,0 +1,5 @@
+package com.codurance
+
+fun scoreFor(allRolls: String): Int {
+ return 0;
+}
---
+++ src/test/kotlin/com/codurance/BowlingShould.kt
@@ -0,0 +1,12 @@
+package com.codurance
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+
+class BowlingShould {
+    @Test
+    fun calculate_score_for_pins() {
+        assertThat(scoreFor(""), `is`(0));
+    }
+}
