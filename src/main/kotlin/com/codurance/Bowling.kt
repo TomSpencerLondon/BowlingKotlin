22f956a43ba41d0cc4a752f29779b8b506698824
Tom <thomas.spencer@codurance.com>
2020-09-08 18:46:22 +0100

    Delete comments

--- src/main/kotlin/com/codurance/Bowling.kt
+++ src/main/kotlin/com/codurance/Bowling.kt
@@ -4,13 +4,6 @@ fun scoreFor(allRolls: String): Int {
  return allRolls.fold(0) { score, roll ->
   score + score(roll)
  }
-
-// var score = 0
-// for (roll in allRolls) {
-//  score += score(roll)
-// }
-//
-// return score
 }
 
 private fun score(roll: Char) =
