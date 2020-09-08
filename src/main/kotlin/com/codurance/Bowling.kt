892cace433508fc45f330dd26a8defaf74d96602
Tom <thomas.spencer@codurance.com>
2020-09-08 18:56:28 +0100

    Add SPARE and MISS for more clarity

--- src/main/kotlin/com/codurance/Bowling.kt
+++ src/main/kotlin/com/codurance/Bowling.kt
@@ -1,22 +1,25 @@
 package com.codurance
 
+private const val MISS = '-'
+private const val SPARE = '/'
+
 fun scoreFor(allRolls: String): Int =
         allRolls.foldIndexed(0) { idx, score, roll ->
             score + score(roll) - spareDiff(allRolls, roll, idx)
         }
 
 fun spareDiff(allRolls: String, roll: Char, idx: Int): Int {
-    return if (roll == '/') score(allRolls.previous(idx)) else 0
+    return if (roll == SPARE) score(allRolls.previous(idx)) else 0
 }
 
 private fun String.previous(idx: Int): Char {
-    return if (idx > 0) this[idx - 1] else '-'
+    return if (idx > 0) this[idx - 1] else MISS
 }
 
 private fun score(roll: Char) =
         when (roll) {
             in '1'..'9' -> String(charArrayOf(roll)).toInt()
-            '/' -> 10
+            SPARE -> 10
             else -> 0
         }
 
